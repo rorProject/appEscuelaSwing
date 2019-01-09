bfa38ccec87374e7e3d4b94757371a969f9610d7
Eric Gaston Haas <ericgaston1234@gmail.com>
2019-01-09 17:22:45 -0300

    Cambios en formulario Datos
    Formulario de datos cambiado por un Jframe

--- src/Formulario/CapturaHuella.java
+++ src/Formulario/CapturaHuella.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package Formulario;
-import Formulario.FormularioDatos;
 import BDD.ConexionBDD;
 import com.digitalpersona.onetouch.DPFPDataPurpose;
 import com.digitalpersona.onetouch.DPFPFeatureSet;
